4722e8a36c2d59681ea4cb8f283bc0dc3dc2951b
z3by <zoaby.am@gmail.com>
2020-01-22 22:03:54 +0200

    add flex search

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const removeMarkdown = require('remove-markdown')
+const { path } = require('@vuepress/shared-utils')
 
-module.exports = () => ({
+module.exports = (options) => ({
     extendPageData ($page) {
         const {
             _content
         } = $page
         $page.content = removeMarkdown(_content)
-    }
+    },
+    alias: {
+        '@SearchBox':
+            path.resolve(__dirname, 'SearchBox.vue')
+    },
 })
