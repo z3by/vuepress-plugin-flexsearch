d14ab13077890502213ce6b4814bf4efb3de74dc
Fabian Streitel <streitel@cqse.eu>
2020-04-18 12:38:48 +0200

    don't search in markdown, search in the rendered page
    fixes #13

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const { path } = require('@vuepress/shared-utils')
 
 module.exports = (options) => ({
     extendPageData ($page) {
-        const {
-            _content
-        } = $page
-        $page.content = _content.replace(/<[^>]+>/g, " ")
+        // _strippedContent does not contain the YAML frontmatter
+        const { html } = $page._context.markdown.render($page._strippedContent)
+        const text = html
+            .replace(/<[^>]+>/g, "") // remove HTML tags
+            .replace(/^\s*#\s/gm, "") // remove header anchors inserted by vuepress
+        $page.content = text
     },
     alias: {
         '@SearchBox':
