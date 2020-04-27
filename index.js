143e244c885c580e9a4d2125e3794ff091a77d45
Fabian Streitel <streitel@cqse.eu>
2020-04-27 08:48:58 +0200

    Decode HTML entities for previews and search index
    fixes #15
    
    we use the "he" library for this as the stripping has to happen at build
    time in `extendPageData`, i.e. in a NodeJS process that does not have
    access to the browser facilities for converting HTML entities.
    
    We don't just strip the entities as depending on the language, they may
    be important searchable characters (e.g. special non-ASCII letters of
    the language).

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { path } = require('@vuepress/shared-utils')
+const he = require('he');
 
 module.exports = (options) => ({
     extendPageData ($page) {
@@ -7,9 +8,11 @@ module.exports = (options) => ({
         }
         // _strippedContent does not contain the YAML frontmatter
         const { html } = $page._context.markdown.render($page._strippedContent)
-        const text = html
-            .replace(/(<[^>]+>)+/g, " ") // remove HTML tags
-            .replace(/^\s*#\s/gm, "") // remove header anchors inserted by vuepress
+        const text = he.decode( // decode HTML entities like &quot;
+			html
+				.replace(/(<[^>]+>)+/g, " ") // remove HTML tags
+				.replace(/^\s*#\s/gm, "") // remove header anchors inserted by vuepress
+		)
         $page.content = text
     },
     alias: {
